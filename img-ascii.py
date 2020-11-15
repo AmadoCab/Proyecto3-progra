dde304e71dbdf3cfa5cde8312e04166e11165c7b
AmadoCab <28881001+AmadoCab@users.noreply.github.com>
2020-11-14 21:16:02 -0600

    muchas cosas

--- img-ascii.py
+++ img-ascii.py
@@ -1,13 +1,20 @@
-from PIL import Image, ImageColor, ImageDraw, ImageOps
+from PIL import Image, ImageColor, ImageDraw, ImageOps, ImageFont
 import os
 import numpy as np
+import datetime
 
-chars = list('@NB#XV±IF*=~-:.·')
+chars = {
+    '4ch' : list('@XF-'),
+    '8ch' : list('@BX±F=-.'),
+    '16ch' : list('@NB#XV±IF*=~-:.·'),
+}
 
 class ImgToAscii:
     # Properties
-    def __init__(self, imgfile, scale=0.5):
+    def __init__(self, imgfile, scale=0.5, quality='16ch'):
         actual_dir = os.getcwd()
+        self.quality = quality
+        self.divide = 2**8//len(chars.get(quality))
         self.imgpath = os.path.join(actual_dir, imgfile)
         self.input_img = Image.open(self.imgpath)
         self.width, self.height = self.input_img.size
@@ -21,7 +28,7 @@ class ImgToAscii:
         print(type(self.working_img))
 
     def rescale(self):
-        self.working_img = self.working_img.resize((int(self.width*17/11*self.scale), int(self.height*11/17*self.scale)))
+        self.working_img = self.working_img.resize((int(self.width*17/10.9*self.scale), int(self.height*10.9/17*self.scale)))
         self.rewidth, self.reheight = self.working_img.size
         self.working_img.save('salida_resize.jpg')
 
@@ -30,21 +37,31 @@ class ImgToAscii:
         self.matrix = np.asarray(self.working_img).transpose()
         for i in range(self.reheight):
             for j in range(self.rewidth):
-                self.imgstr += chars[self.matrix[j, i]//16]
+                self.imgstr += chars.get(self.quality)\
+                    [self.matrix[j, i]//self.divide]
             self.imgstr += '\n'
 
     def to_print(self):
         print(self.imgstr)
 
     def to_doc(self):
-        with open('salida.txt', 'w') as f:
+        with open(f'{datetime.datetime.now()}.txt', 'w') as f:
             f.write(self.imgstr)
 
+    def to_pic(self):
+        imgwidth, imgheight = self.matrix.shape
+        txtimg = Image.new('L', (imgwidth*7,imgheight*17), color=2**8)
+        draw = ImageDraw.Draw(txtimg)
+        fnt = ImageFont.truetype('/Users/Macbook/Desktop/Python/PrograM/Proyecto 3/Font/IBMPlexMono-Medium.ttf',12)
+        draw.multiline_text((0,0), self.imgstr, fill=0, font=fnt)
+        txtimg.save('dibujoletras.jpg')
+
 if __name__ == "__main__":
-    imagen = ImgToAscii('Amadito.jpeg',0.3)
+    imagen = ImgToAscii('Images/cont.jpeg',0.3,'16ch')
     imagen.to_blackwhite()
     imagen.rescale()
     imagen.imgmatrix()
     imagen.to_print()
+    imagen.to_pic()
 
 #
