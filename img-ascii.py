c78406a2b8482618c3c1e6e2f75a369d01444210
AmadoCab <28881001+AmadoCab@users.noreply.github.com>
2020-11-19 19:23:50 -0600

    no se que cenar

--- img-ascii.py
+++ img-ascii.py
@@ -6,12 +6,14 @@ import datetime
 import tweepy
 import os
 
+### List of characters to generate an image ###
 chars = {
     '4ch' : list('@XF-'),
     '8ch' : list('@BX±F=-.'),
     '16ch' : list('@NB#XV±IF*=~-:.·'),
 }
 
+### Class to generate the images and text ###
 class ImgToAscii:
     # Properties
     def __init__(self, imgfile, scale=0.5, quality='16ch'):
@@ -63,15 +65,45 @@ class ImgToAscii:
         consumer_key = config('API_KEY')
         consumer_secret = config('API_SECRET_KEY')
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        webbrowser.open(auth.get_authorization_url())
-        pin = input('Verification pin number from twitter.com: ').strip()
-        token = auth.get_access_token(verifier=pin)
-        access_token = token[0]
-        access_token_secret = token[1]
-        auth.set_access_token(access_token, access_token_secret)
-        api = tweepy.API(auth)
+        if not is_logged():
+            webbrowser.open(auth.get_authorization_url())
+            pin = input('Verification pin number from twitter.com: ').strip()
+            token = auth.get_access_token(verifier=pin)
+            access_token = token[0]
+            access_token_secret = token[1]
+            auth.set_access_token(access_token, access_token_secret)
+            api = tweepy.API(auth)
+            login(api.me()._json.get('name'), token[0], token[1])
+        else:
+            access_token, access_token_secret = get_credentials()
+            auth.set_access_token(access_token, access_token_secret)
+            api = tweepy.API(auth)
         loqeusea = api.media_upload('dibujoletras.jpg')
         api.update_status('',media_ids=[loqeusea.media_id])
+
+def is_logged():
+    """
+    más tardecito escribo
+    """
+    with open('credentials.txt', 'r') as f:
+        if f.readline() == 'NOCREDENTIALS':
+            return False
+        else:
+            return True
+
+def get_credentials():
+    with open('credentials.txt', 'r') as f:
+        linea = f.readline().strip().split(',')
+    return linea[1], linea[2]
+
+def login(nombre, key, skey):
+    cadena = f'{nombre},{key},{skey}'
+    with open('credentials.txt', 'w') as f:
+        f.write(cadena)
+
+def logout():
+    with open('credentials.txt', 'w') as f:
+        f.write('NOCREDENTIALS')
 
 if __name__ == "__main__":
     imagen = ImgToAscii('Images/Kanis.jpeg',0.3,'16ch')
